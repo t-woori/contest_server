0810c1b773b47eeb6b1ec5b56be9513ad7f43d55
aronalee <aronalee10@gmail.com>
2023-07-25 16:45:42 +0900

    feat: 대회 관련 jpa 추가
    대회 기능에 관련된 JPA들을 추가
    - Contest.java: 대회 정보를 가진 entity
    - Problem.java: 문제 정보를 가진 entity
    - Sequence.java: 문제 내의 질문, 응답 정보를 가진 entity
    - ProblemInContest.java: 대회에서 사용할 문제 매핑 정보를 가진 entity

---
+++ src/main/java/com/twoori/contest_server/domain/contest/dao/ProblemInContest.java
@@ -0,0 +1,37 @@
+package com.twoori.contest_server.domain.contest.dao;
+
+import com.twoori.contest_server.domain.problem.dao.Problem;
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@Table(name = "problem_in_contest")
+public class ProblemInContest {
+    @Id
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "contest_id", nullable = false)
+    private Contest contest;
+
+    @Id
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "problem_id", nullable = false)
+    private Problem problem;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+    private Integer noOfProblemInContest;
+
+
+}
