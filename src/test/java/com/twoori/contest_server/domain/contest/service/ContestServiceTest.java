1b45894185b0921501ee96ceaf44d6d94444f4f7
aronalee <aronalee10@gmail.com>
2023-08-09 05:37:15 +0900

    test: 대회 참여 테스트 코드 변경
    - 변경 전: 대회 시간만 판별
    - 변경 후: 학생이 대회에 참가할 수 있는 자격을 갖췄는지 확인

--- src/test/java/com/twoori/contest_server/domain/contest/service/ContestServiceTest.java
+++ src/test/java/com/twoori/contest_server/domain/contest/service/ContestServiceTest.java
@@ -1,7 +1,12 @@
 package com.twoori.contest_server.domain.contest.service;
 
 import com.twoori.contest_server.domain.contest.dao.Contest;
-import com.twoori.contest_server.domain.contest.repository.ContestRepository;
+import com.twoori.contest_server.domain.contest.dto.ContestDTO;
+import com.twoori.contest_server.domain.contest.exception.EarlyContestException;
+import com.twoori.contest_server.domain.contest.exception.ExpiredContestException;
+import com.twoori.contest_server.domain.student.dao.Student;
+import com.twoori.contest_server.domain.student.dao.StudentInContest;
+import com.twoori.contest_server.domain.student.repository.StudentInContestRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -14,6 +19,8 @@ import java.util.Optional;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 @ExtendWith({MockitoExtension.class})
@@ -22,65 +29,75 @@ class ContestServiceTest {
     public static final int CONTEST_TIME = 15;
     @InjectMocks
     private ContestService contestService;
+    private final Student student = Student.builder()
+            .id(UUID.randomUUID())
+            .build();
     @Mock
-    private ContestRepository contestRepository;
-
+    private StudentInContestRepository studentInContestRepository;
 
     @DisplayName("Fail case1: 대회가 종료된 후 입장 시도")
     @Test
-    void givenLateTimeWhenIsEnterContestThenFalse() {
-        LocalDateTime now = LocalDateTime.now();
+    void givenEndContestWhenGetAccessibleContestInformationThenThrowExpiredContestException() {
         // given
-        LocalDateTime lateDateTime = now.plusMinutes(CONTEST_TIME + 1);
-        // when
         UUID contestId = UUID.randomUUID();
-        when(contestRepository.findById(contestId)).thenReturn(Optional.of(Contest.builder()
+        LocalDateTime startDateTime = LocalDateTime.now();
+        LocalDateTime endDateTime = startDateTime.plusMinutes(CONTEST_TIME);
+        LocalDateTime enterDateTime = endDateTime.plusMinutes(1);
+        Contest endContest = Contest.builder()
                 .id(contestId)
-                .runningStartDateTime(now)
-                .runningEndDateTime(now.plusMinutes(CONTEST_TIME))
-                .build()));
-        boolean result = contestService.isEnterContest(contestId, lateDateTime);
-
-        //then
-        assertThat(result).isFalse();
+                .runningStartDateTime(startDateTime)
+                .runningEndDateTime(endDateTime)
+                .build();
+        StudentInContest studentInContest = StudentInContest.builder().student(student).contest(endContest).build();
+        given(studentInContestRepository.findByContest_IdAndStudent_Id(contestId, student.getId())).willReturn(Optional.of(studentInContest));
+        // when & then
+        assertThatThrownBy(() -> contestService.getAccessibleContest(student.getId(), contestId, enterDateTime))
+                .isInstanceOf(ExpiredContestException.class);
     }
 
     @DisplayName("Fail case2: 대회 대기 시간 전에 입장 시도")
     @Test
-    void givenEarlyTimeWhenIsEnterContestThenFalse() {
-        LocalDateTime now = LocalDateTime.now();
+    void givenEarlyContestWhenGetAccessibleContestInformationThenThrowEarlyContestException() {
         // given
-        LocalDateTime earlyTime = now.minusMinutes(ENTER_TIME + 1);
-        // when
         UUID contestId = UUID.randomUUID();
-        when(contestRepository.findById(contestId)).thenReturn(Optional.of(Contest.builder()
+        LocalDateTime startDateTime = LocalDateTime.now();
+        LocalDateTime endDateTime = startDateTime.plusMinutes(CONTEST_TIME);
+        LocalDateTime enterDateTime = startDateTime.minusMinutes(ENTER_TIME + 1);
+        Contest earlyContest = Contest.builder()
                 .id(contestId)
-                .runningStartDateTime(now)
-                .runningEndDateTime(now.plusMinutes(CONTEST_TIME))
-                .build()));
-        boolean result = contestService.isEnterContest(contestId, earlyTime);
-
-        //then
-        assertThat(result).isFalse();
+                .runningStartDateTime(startDateTime)
+                .runningEndDateTime(endDateTime)
+                .build();
+        StudentInContest studentInContest = StudentInContest.builder().student(student).contest(earlyContest).build();
+        // when & then
+        when(studentInContestRepository.findByContest_IdAndStudent_Id(contestId, student.getId())).thenReturn(Optional.of(studentInContest));
+        assertThatThrownBy(() -> contestService.getAccessibleContest(student.getId(), contestId, enterDateTime))
+                .isInstanceOf(EarlyContestException.class);
     }
 
     @DisplayName("Success case: 대회 시작 10분전에 입장 시도")
     @Test
-    void givenEnterTimeWhenIsEnterContestThenTrue() {
-        LocalDateTime now = LocalDateTime.now();
+    void givenContestWhenGetAccessibleContestInformationThenContestDTO() {
         // given
-        LocalDateTime enterTime = now.minusMinutes(ENTER_TIME);
+        UUID contestId = UUID.randomUUID();
+        LocalDateTime startDateTime = LocalDateTime.now();
+        LocalDateTime endDateTime = startDateTime.plusMinutes(CONTEST_TIME);
+        LocalDateTime enterDateTime = startDateTime.minusMinutes(ENTER_TIME);
+        Contest contest = Contest.builder()
+                .id(contestId)
+                .runningStartDateTime(startDateTime)
+                .runningEndDateTime(endDateTime)
+                .build();
+        StudentInContest studentInContest = StudentInContest.builder().student(student).contest(contest).build();
+        given(studentInContestRepository.findByContest_IdAndStudent_Id(contestId, student.getId())).willReturn(Optional.of(studentInContest));
 
         // when
-        UUID contestId = UUID.randomUUID();
-        when(contestRepository.findById(contestId)).thenReturn(Optional.of(Contest.builder()
-                .id(contestId)
-                .runningStartDateTime(now)
-                .runningEndDateTime(now.plusMinutes(CONTEST_TIME))
-                .build()));
-        boolean result = contestService.isEnterContest(contestId, enterTime);
+        ContestDTO contestDTO = contestService.getAccessibleContest(student.getId(), contestId, enterDateTime);
 
-        //then
-        assertThat(result).isTrue();
+        // then
+        assertThat(contestDTO)
+                .extracting("id", "runningStartDateTime", "runningEndDateTime")
+                .doesNotContainNull()
+                .containsExactly(contestId, startDateTime, endDateTime);
     }
 }
