368d2eca0cefec74f4f6360ef99f0f79338da7fd
aronalee <aronalee10@gmail.com>
2023-09-28 16:39:18 +0900

    fix: redisConnectionFactory 주입 방식으로 변경

--- src/main/java/com/twoori/contest_server/global/config/RedisConfig.java
+++ src/main/java/com/twoori/contest_server/global/config/RedisConfig.java
@@ -1,28 +1,31 @@
 package com.twoori.contest_server.global.config;
 
 import com.twoori.contest_server.domain.problem.dto.ProblemDto;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+
+    private final RedisConnectionFactory redisConnectionFactory;
+
+    @Autowired
+    public RedisConfig(RedisConnectionFactory redisConnectionFactory) {
+        this.redisConnectionFactory = redisConnectionFactory;
     }
 
     @Bean
     public RedisTemplate<String, ProblemDto> redisTemplate() {
         RedisTemplate<String, ProblemDto> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new Jackson2JsonRedisSerializer<>(String.class));
-
+        /**/
         return redisTemplate;
     }
 }
