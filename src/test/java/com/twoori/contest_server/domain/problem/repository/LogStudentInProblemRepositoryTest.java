e983d94c622bcee90695a0ab22a2281e84a94b3a
aronalee <aronalee10@gmail.com>
2023-10-03 20:18:40 +0900

    test: 채점 결과 테스트 코드 추가
    - service: 더 높은 점수를 선택해서 캐싱을 진행하는 여부
    - repository: 문제의 최대 점수 조회, 문제 푼 기록 카운트 테스트
    - controller는 kinesis연동 후 작성

---
+++ src/test/java/com/twoori/contest_server/domain/problem/repository/LogStudentInProblemRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.twoori.contest_server.domain.problem.repository;
+
+import com.twoori.contest_server.domain.problem.dao.LogStudentInProblem;
+import com.twoori.contest_server.domain.problem.dao.LogStudentInProblemID;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ActiveProfiles("test")
+@Transactional
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class LogStudentInProblemRepositoryTest {
+
+    @Autowired
+    private TestEntityManager em;
+
+    @Autowired
+    private LogStudentInProblemRepository logStudentInProblemRepository;
+
+    @DisplayName("학생이 문제 푼 기록이 count 건 있다.|Success| count를 반환한다")
+    @ValueSource(ints = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11})
+    @ParameterizedTest(name = "문제 푼 건수: {0}")
+    void givenExistSolvedProblemVoWhenSaveSolvedProblemThenSuccess(int count) {
+        // given
+        UUID contestId = UUID.fromString("53a70353-1f96-4b39-84f9-22704218627f");
+        UUID studentId = UUID.fromString("d7762394-592c-4e33-8d71-06fc5a94abfb");
+        Long problemId = 1L;
+        Long contentId = 0L;
+        Double score = 0.01;
+        for (int i = 0; i < count; i++) {
+            logStudentInProblemRepository.saveAndFlush(new LogStudentInProblem(
+                    LogStudentInProblemID.ofIncludeCountOfTry(
+                            contestId, studentId, problemId, contentId, i + 1)
+                    , score));
+        }
+
+        // when
+        Integer maxCount = logStudentInProblemRepository.countLatestSolvedProblem(
+                LogStudentInProblemID.ofExcludeCountOfTry(contestId, studentId, problemId, contentId));
+
+        // then
+        assertThat(maxCount).isEqualTo(count);
+    }
+
+    @DisplayName("문제를 푼 기록 중에 최고 점수 반환|Success|최고 점수 반환")
+    @ValueSource(doubles = {0.8, 0.7, 0.6, 0.5})
+    @ParameterizedTest(name = "최고 점수: {0}")
+    void givenMaxScoreDecs0_01WhenGetMAxScoreProblemOneThenReturnMaxScore(double maxScore) {
+        // given
+        UUID contestId = UUID.fromString("53a70353-1f96-4b39-84f9-22704218627f");
+        UUID studentId = UUID.fromString("d7762394-592c-4e33-8d71-06fc5a94abfb");
+        Long problemId = 1L;
+        Long contentId = 0L;
+        for (int i = 0; i < 3; i++) {
+            logStudentInProblemRepository.saveAndFlush(new LogStudentInProblem(
+                    LogStudentInProblemID.ofIncludeCountOfTry(
+                            contestId, studentId, problemId, contentId, i + 1)
+                    , maxScore - i * 0.01));
+        }
+
+        // when
+        Double actual = logStudentInProblemRepository.getMaxScoreProblemOne(
+                LogStudentInProblemID.ofExcludeCountOfTry(contestId, studentId, problemId, contentId));
+
+        // then
+        assertThat(actual).isEqualTo(maxScore);
+    }
+
+}
--- src/test/java/com/twoori/contest_server/domain/problem/service/ProblemServiceTest.java
+++ src/test/java/com/twoori/contest_server/domain/problem/service/ProblemServiceTest.java
@@ -1,13 +1,18 @@
 package com.twoori.contest_server.domain.problem.service;
 
+import com.twoori.contest_server.domain.problem.dao.LogStudentInProblem;
+import com.twoori.contest_server.domain.problem.dao.LogStudentInProblemID;
 import com.twoori.contest_server.domain.problem.dto.ContentDto;
 import com.twoori.contest_server.domain.problem.dto.ProblemDto;
 import com.twoori.contest_server.domain.problem.enums.CHAPTER_TYPE;
 import com.twoori.contest_server.domain.problem.enums.GRADE;
 import com.twoori.contest_server.domain.problem.enums.PROBLEM_TYPE;
 import com.twoori.contest_server.domain.problem.exceptions.NotFoundProblemException;
+import com.twoori.contest_server.domain.problem.repository.LogStudentInProblemRepository;
 import com.twoori.contest_server.domain.problem.repository.ProblemRepository;
+import com.twoori.contest_server.domain.problem.vo.SolvedProblemVO;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -18,9 +23,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.util.List;
 import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
 class ProblemServiceTest {
@@ -29,6 +37,9 @@ class ProblemServiceTest {
     private ProblemService problemService;
     @Mock
     private ProblemRepository problemRepository;
+    @Mock
+    private LogStudentInProblemRepository logStudentInProblemRepository;
+
 
     @DisplayName("대회 ID와 문제 ID를 받아서 문제를 제공|Success|문제 제공 성공")
     @MethodSource("com.twoori.contest_server.domain.problem.repository.Parameters#parametersOfExistsProblemId")
@@ -58,4 +69,87 @@ class ProblemServiceTest {
 
     }
 
+    @DisplayName("처음으로 푸는 문제|Cache Put|RDB에 저장 하고 redis에 캐싱")
+    @Test
+    void givenNewSolvedProblemVOWhenUpdateScoreThenUpdateMaxScore() {
+        // given
+        UUID contestId = UUID.randomUUID();
+        UUID studentId = UUID.randomUUID();
+        Long noOfProblemInContest = 0L;
+        Long contentId = 0L;
+        Double newScore = 0.70;
+        SolvedProblemVO solvedProblemVO = new SolvedProblemVO(studentId, contestId, noOfProblemInContest, contentId, newScore);
+        given(logStudentInProblemRepository.countLatestSolvedProblem(
+                LogStudentInProblemID.ofExcludeCountOfTry(contestId, studentId, noOfProblemInContest, solvedProblemVO.contentId())
+        )).willReturn(0);
+
+        // when
+        Double actual = problemService.updateMaxScoreAboutProblem(solvedProblemVO);
+
+        // then
+        verify(logStudentInProblemRepository).save(
+                new LogStudentInProblem(
+                        LogStudentInProblemID.ofIncludeCountOfTry(contestId, studentId, noOfProblemInContest, solvedProblemVO.contentId(), 1)
+                        , solvedProblemVO.newScore()));
+        assertThat(actual).isEqualTo(newScore);
+    }
+
+    @DisplayName("기존 점수보다 새로운 문제 점수가 높다|Not Cache Put|RDB에 저장하고 redis에 캐싱")
+    @Test
+    void givenNewScoreWhenUpdateScoreThenUpdateMaxScore() {
+        // given
+        UUID contestId = UUID.randomUUID();
+        UUID studentId = UUID.randomUUID();
+        Long noOfProblemInContest = 0L;
+        Long contentId = 0L;
+        Double newScore = 0.70;
+        LogStudentInProblemID id = LogStudentInProblemID.ofExcludeCountOfTry(contestId, studentId, noOfProblemInContest, contentId);
+        SolvedProblemVO solvedProblemVO = new SolvedProblemVO(studentId, contestId, noOfProblemInContest, contentId, newScore);
+        given(logStudentInProblemRepository.countLatestSolvedProblem(id)).willReturn(1);
+        given(logStudentInProblemRepository.getMaxScoreProblemOne(id)).willReturn(0.60);
+
+        // when
+        Double actual = problemService.updateMaxScoreAboutProblem(solvedProblemVO);
+
+        // then
+        Integer nextCountOfTry = 0;
+        LogStudentInProblemID insertedEntityId = LogStudentInProblemID.ofIncludeCountOfTry(contestId,
+                studentId,
+                noOfProblemInContest,
+                contentId,
+                nextCountOfTry);
+        assertThat(actual).isEqualTo(newScore);
+        verify(logStudentInProblemRepository, times(1)).save(
+                new LogStudentInProblem(insertedEntityId, solvedProblemVO.newScore()));
+    }
+
+    @DisplayName("기존 점수보다 새로운 점수가 더 낮다|Not Cache Put|RDB에 로그만 기록하고 rediss에 캐싱하지 않는다.")
+    @Test
+    void givenNewScoreWhenUpdateScoreThenNotUpdateMaxScore() {
+        // given
+        UUID contestId = UUID.randomUUID();
+        UUID studentId = UUID.randomUUID();
+        Long noOfProblemInContest = 0L;
+        Long contentId = 0L;
+        Double beforeMaxScore = 0.70;
+        Double newScore = 0.60;
+        LogStudentInProblemID id = LogStudentInProblemID.ofExcludeCountOfTry(contestId, studentId, noOfProblemInContest, contentId);
+        SolvedProblemVO solvedProblemVO = new SolvedProblemVO(studentId, contestId, noOfProblemInContest, contentId, newScore);
+        given(logStudentInProblemRepository.countLatestSolvedProblem(id)).willReturn(1);
+        given(logStudentInProblemRepository.getMaxScoreProblemOne(id)).willReturn(beforeMaxScore);
+
+        // when
+        Double actual = problemService.updateMaxScoreAboutProblem(solvedProblemVO);
+
+        // then
+        Integer nextCountOfTry = 1;
+        LogStudentInProblemID insertedEntityId = LogStudentInProblemID.ofIncludeCountOfTry(contestId,
+                studentId,
+                noOfProblemInContest,
+                contentId,
+                nextCountOfTry);
+        assertThat(actual).isEqualTo(beforeMaxScore);
+        verify(logStudentInProblemRepository, times(1)).save(
+                new LogStudentInProblem(insertedEntityId, solvedProblemVO.newScore()));
+    }
 }
