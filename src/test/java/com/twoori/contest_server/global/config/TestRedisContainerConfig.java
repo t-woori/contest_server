adbba0331b976973fb8167b9d99ab5a3021d291b
aronalee <aronalee10@gmail.com>
2023-10-18 17:31:37 +0900

    chore: 안쓰는 설정 제거
    - ActiveProfile은 설정에 무관하므로 제거

--- src/test/java/com/twoori/contest_server/global/config/TestRedisContainerConfig.java
+++ src/test/java/com/twoori/contest_server/global/config/TestRedisContainerConfig.java
@@ -3,12 +3,10 @@ package com.twoori.contest_server.global.config;
 import org.junit.jupiter.api.extension.BeforeAllCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.test.context.ActiveProfiles;
 import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.utility.DockerImageName;
 
 
-@ActiveProfiles("test")
 @TestConfiguration
 public class TestRedisContainerConfig implements BeforeAllCallback {
     private static final String REDIS_IMAGE = "redis:7.0.8-alpine";
