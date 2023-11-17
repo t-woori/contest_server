1e83ea31e564cf13789cb1f95dbcdcff50414fe4
aronalee <aronalee10@gmail.com>
2023-11-17 14:37:54 +0900

    refactor: 상태 VO객체 위치 변경
    - 학생의 상태에 포함되는 정보이나 정보의 본질은 contest, problem이므로 위치를 옮김

--- src/main/java/com/twoori/contest_server/domain/student/vo/StudentStatusVO.java
+++ src/main/java/com/twoori/contest_server/domain/student/vo/StudentStatusVO.java
@@ -1,6 +1,7 @@
 package com.twoori.contest_server.domain.student.vo;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.twoori.contest_server.domain.contest.vo.ContestStatusVo;
 
-public record StudentStatusVO(@JsonProperty("running_contest") ContestStatus contestStatus) {
+public record StudentStatusVO(@JsonProperty("running_contest") ContestStatusVo contestStatusVO) {
 }
